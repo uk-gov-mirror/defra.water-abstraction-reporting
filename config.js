51bf483f128ef80e24902451918216c72e4bdb88
Tim <tim@tamimi.dev>
2020-12-16 17:13:26 +0000

    Added S3 and Redis connectors

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const testMode = parseInt(process.env.TEST_MODE) === 1;
 const isAcceptanceTestTarget = ['local', 'dev', 'development', 'test', 'qa', 'preprod'].includes(process.env.NODE_ENV);
 
+const isLocal = process.env.NODE_ENV === 'local';
+
 module.exports = {
   blipp: {
     showAuth: true
@@ -20,10 +22,31 @@ module.exports = {
     max: 5
   },
 
+  s3: {
+    accessKeyId: process.env.S3_KEY,
+    secretAccessKey: process.env.S3_SECRET,
+    region: 'eu-west-1',
+    bucket: process.env.S3_BUCKET
+  },
+
   server: {
     port: 8009,
     router: {
       stripTrailingSlash: true
+    }
+  },
+
+  redis: {
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD || '',
+    ...!(isLocal) && { tls: {} },
+    db: 2
+  },
+
+  good: {
+    ops: {
+      interval: 10000
     }
   },
 
