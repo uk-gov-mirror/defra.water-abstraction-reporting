3d433ed7dc060b5f1218c1571f8eab518d0c06a8
Tim <tim@tamimi.dev>
2020-12-16 15:37:50 +0000

    Add ESlint and lab and a CI pipeline

--- config.js
+++ config.js
@@ -2,30 +2,30 @@ const testMode = parseInt(process.env.TEST_MODE) === 1;
 const isAcceptanceTestTarget = ['local', 'dev', 'development', 'test', 'qa', 'preprod'].includes(process.env.NODE_ENV);
 
 module.exports = {
-    blipp: {
-        showAuth: true
-    },
-    jwt: {
-        key: process.env.JWT_SECRET,
-        verifyOptions: { algorithms: ['HS256'] }
-    },
-    logger: {
-        level: testMode ? 'info' : 'error',
-        airbrakeKey: process.env.ERRBIT_KEY,
-        airbrakeHost: process.env.ERRBIT_SERVER,
-        airbrakeLevel: 'error'
-    },
-    pg: {
-        connectionString: process.env.DATABASE_URL,
-        max: 5,
-    },
+  blipp: {
+    showAuth: true
+  },
+  jwt: {
+    key: process.env.JWT_SECRET,
+    verifyOptions: { algorithms: ['HS256'] }
+  },
+  logger: {
+    level: testMode ? 'info' : 'error',
+    airbrakeKey: process.env.ERRBIT_KEY,
+    airbrakeHost: process.env.ERRBIT_SERVER,
+    airbrakeLevel: 'error'
+  },
+  pg: {
+    connectionString: process.env.DATABASE_URL,
+    max: 5
+  },
 
-    server: {
-        port: 8009,
-        router: {
-            stripTrailingSlash: true
-        }
-    },
+  server: {
+    port: 8009,
+    router: {
+      stripTrailingSlash: true
+    }
+  },
 
-    isAcceptanceTestTarget
+  isAcceptanceTestTarget
 };
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 const controllers = require('./controllers');
 
 const routes = [{
-    method: 'GET',
-    handler: controllers.getStatus,
-    options: {
-        auth: false,
-        description: 'Checks if the service is alive'
-    },
-    path: '/status'
+  method: 'GET',
+  handler: controllers.getStatus,
+  options: {
+    auth: false,
+    description: 'Checks if the service is alive'
+  },
+  path: '/status'
 }];
 
 module.exports = routes;
